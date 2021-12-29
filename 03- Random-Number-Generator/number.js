7930dba285280b0c5c977f789e4360e4b05343db
Bhavya <bhavya.sachdeva.58@gmail.com>
2021-12-29 19:01:04 +0530

    Day-4 Complete

--- 03- Random-Number-Generator/number.js
+++ 03- Random-Number-Generator/number.js
@@ -1,7 +1,7 @@
 const count = document.querySelector(".count");
 const number = document.querySelector(".number_button");
- const randomNumber = () => {
- const random = Math.floor(Math.random()*100+1);
+const randomNumber = () => {
+const random = Math.floor(Math.random()*100+1);
  count.innerHTML = random;
  };
  number.addEventListener("click", randomNumber);
